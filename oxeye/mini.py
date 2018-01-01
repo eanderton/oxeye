ba250e56a0451056b7e5af4bd90f8be0573954af
eanderton <eric.t.anderton@gmail.com>
2018-01-01 14:17:19 -0500

    added mini parser

---
+++ oxeye/mini.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+'''
+This mini-implementation of Oxeye is provided as a starting point
+for implementing state-machine based grammars using a similar
+approach.  The implementation itself is small enough to be easily
+vendored (cut-and-paste) into another program, which may be
+desireable over an entire module install and import.
+
+If `oxeye.mini.parse` is too limiting, please see
+`oxeye.parser.Parser` for a more feature-rich approach to this 
+style of state machine.
+
+>>> text = """
+... Not comment text
+... # hello
+... # commented
+... Also not comment text
+... # world
+... """
+>>> nop = lambda: None 
+>>> comments = []
+>>> parse({
+...     'goal': (
+...         ('#\\s+(.*)\\n', comments.append, 'goal'),
+...         ('.*\\n', nop, 'goal'),
+...     ),
+... }, text)
+(u'goal', 68)
+
+>>> print comments
+[u'hello', u'commented', u'world']
+
+'''
+
+from __future__ import unicode_literals, absolute_import
+
+import re
+
+
+def parse(spec, text, state='goal', pos=0):
+    '''
+    This function processes `text` against `spec` state-machine 
+    specification, starting at the specified `state` and optional `pos` 
+    in text.  
+    
+    The parser runs to exhaustion, or when no more states can be 
+    matched.  Upon exhausting `test`, a status `(state, pos)` tuple 
+    for the current state and position is returned. 
+
+    The spec is a dictionary of state to rule list mappings, where each
+    rule is a `(regular_expression, predicate_function, next_state_)`, 
+    tuple.
+
+    When a regular expression is matched, the match groups are passed
+    as `*args`, and the named matches as `**kwargs`, to the predicate
+    function.
+    
+    See `oxeye.pred` for predicate function examples to use with this
+    parser.
+    '''
+    while pos < len(text):
+        for rex, fn, next_state in spec[state]:
+            result = re.match(rex, text[pos:])
+            if result:
+                fn(*result.groups(), **result.groupdict())
+                pos += result.end()
+                state = next_state
+                break
+        else:
+            break
+    return state, pos
